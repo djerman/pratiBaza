57d42c1588974045ae9caba18dab2df9b6237a67
goran <atekom.rs@gmail.com>
2022-10-08 15:24:27 +0200

    novi test

--- src/test/java/pratiBaza/daoImpl/ProracunPuta.java
+++ src/test/java/pratiBaza/daoImpl/ProracunPuta.java
@@ -1,6 +1,8 @@
 package pratiBaza.daoImpl;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +10,11 @@ class ProracunPuta {
 
 	@Test
 	void test() {
-		fail("Not yet implemented but maybe latter");
-		
+		//fail("Not yet implemented but maybe latter");
+	    List<Integer> numbers = Arrays.asList(1, 2, 3);
+	    assertTrue(numbers.stream()
+	      .mapToInt(Integer::intValue)
+	      .sum() > 5, () -> "Sum should be greater than 5");
 	}
 
 }
