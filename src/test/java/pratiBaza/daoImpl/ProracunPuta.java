b5f6f61471cb0aab635bcf4a18f3bc4cd7729d4f
goran <atekom.rs@gmail.com>
2022-10-08 12:25:53 +0200

    dodavanje test forldera

---
+++ src/test/java/pratiBaza/daoImpl/ProracunPuta.java
@@ -0,0 +1,14 @@
+package pratiBaza.daoImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProracunPuta {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
